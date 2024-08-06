3b453f7563dc4c59e442d1fa6ee2e0a26e6f31df
MichaelLELU <lelu.michael@gmail.com>
2024-08-07 01:31:19 +0200

    fix: redirection to restricted pages

--- client/src/pages/userpage/UserPage.jsx
+++ client/src/pages/userpage/UserPage.jsx
@@ -11,39 +11,33 @@ export default function UserPage() {
   const { currentUser } = useOutletContext();
   const [videoData, setVideoData] = useState();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (currentUser.role !== "user") {
-        <Navigate to="/" />;
-      }
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [currentUser]);
-
   const express = import.meta.env.VITE_API_URL;
-
   useEffect(() => {
     axios.get(`${express}/api/favorites/${currentUser?.id}`).then((videos) => {
       setVideoData(videos.data);
     });
   }, [express, currentUser?.id]);
 
-  return (
-    <div className="user-page-container">
-      <section className="personal-information">
-        <h1 className="title-info">{currentUser?.firstname} Informations:</h1>
-        <p className="user-details">{`Firstname: ${currentUser?.firstname}`}</p>
-        <p className="user-details">{`Lastname: ${currentUser?.lastname}`}</p>
-        <p className="user-details">{`Email: ${currentUser?.email}`}</p>
-      </section>
-      <section className="update-information">
-        <h2>Update your personal informations</h2>
-        <NameUpdate user={currentUser} />
-      </section>
+  return currentUser === null ? (
+    <Navigate to="/login" />
+  ) : (
+    <>
+      <div className="user-page-container">
+        <section className="personal-information">
+          <h1 className="title-info">{currentUser?.firstname} Informations:</h1>
+          <p className="user-details">{`Firstname: ${currentUser?.firstname}`}</p>
+          <p className="user-details">{`Lastname: ${currentUser?.lastname}`}</p>
+          <p className="user-details">{`Email: ${currentUser?.email}`}</p>
+        </section>
+        <section className="update-information">
+          <h2>Update your personal informations</h2>
+          <NameUpdate user={currentUser} />
+        </section>
+      </div>
       <section>
         <h2>Your Favorits Video</h2>
         <VideoList videoData={videoData} />
       </section>
-    </div>
+    </>
   );
 }
