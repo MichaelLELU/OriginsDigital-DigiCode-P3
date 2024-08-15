d9bedd9d6c74ff073431547a6073f64ec301bd11
MichaelLELU <lelu.michael@gmail.com>
2024-08-15 11:47:30 +0200

    fix: admin wall on readalluser

--- server/app/routers/api/users/router.js
+++ server/app/routers/api/users/router.js
@@ -10,12 +10,14 @@ const {
   add,
   destroy,
 } = require("../../../controllers/userActions");
+
 const hashPassword = require("../../../services/hashPassword");
 const userValidation = require("../../../services/validation/userValidation");
 const userWall = require("../../../services/userWall");
+const adminWall = require("../../../services/adminWall");
 
 // get a list of all users
-router.get("/", browse);
+router.get("/", adminWall, browse);
 
 // route to get a specific user
 router.get("/:id", read);
