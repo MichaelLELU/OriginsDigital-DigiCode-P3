ac329981dc30cbb68f8a337528ba9a5239669edd
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-08-16 17:33:27 +0200

    test(CategoriesPage): removed 'redundant' link to homepage (back button)

--- client/src/pages/categoriespage/CategoriesPage.jsx
+++ client/src/pages/categoriespage/CategoriesPage.jsx
@@ -1,5 +1,4 @@
-import { useLoaderData, NavLink, Link } from "react-router-dom";
-import { ArrowLeftIcon } from "lucide-react";
+import { useLoaderData, NavLink } from "react-router-dom";
 import SearchBar from "../../components/searchbar/SearchBar";
 import Filter from "../../components/filter/Filter";
 import CategoriesList from "../../components/categorieslist/CategoriesList";
@@ -9,34 +8,25 @@ export default function CategoriesPage() {
   const categoriesData = useLoaderData();
 
   return (
-    <>
-      <Link to="/">
-        <button type="button" className="backButton">
-          <ArrowLeftIcon size={18} />
-          Home
-        </button>
-      </Link>
-
-      <div className="categories-container">
-        <div className="search-bar-categories">
-          <SearchBar />
-        </div>
-
-        <div className="filter-categories">
-          <Filter category={categoriesData} />
-        </div>
-
-        {categoriesData.map((c) => (
-          <div key={c.id}>
-            <h1 className="title-categories">
-              <NavLink to={`/categories/${c.name.replaceAll(" ", "-")}`}>
-                {c.name}
-              </NavLink>
-            </h1>
-            <CategoriesList category={c} key={c.id} />
-          </div>
-        ))}
+    <div className="categories-container">
+      <div className="search-bar-categories">
+        <SearchBar />
       </div>
-    </>
+
+      <div className="filter-categories">
+        <Filter category={categoriesData} />
+      </div>
+
+      {categoriesData.map((c) => (
+        <div key={c.id}>
+          <h1 className="title-categories">
+            <NavLink to={`/categories/${c.name.replaceAll(" ", "-")}`}>
+              {c.name}
+            </NavLink>
+          </h1>
+          <CategoriesList category={c} key={c.id} />
+        </div>
+      ))}
+    </div>
   );
 }
