e86f6dad56b1f181cca9ab39ddbcd313eba12b56
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-08-24 09:01:53 +0200

    fix: added userwall to prevent unauthorized modifications to favorites

--- server/app/routers/api/favorite/router.js
+++ server/app/routers/api/favorite/router.js
@@ -9,16 +9,21 @@ const {
   allFavorites,
 } = require("../../../controllers/favoriteController");
 
+const userWall = require("../../../services/userWall");
+
 // check if a video is favorited
 router.get("/check/:userId/:videoId", checkFavorite);
+
+// get all favorites
+router.get("/:userId", allFavorites);
+
+// secure routes with userWall
+router.use(userWall);
 
 // add a video to favorites
 router.post("/:videoId", addFavorite);
 
 // remove a video from favorites
 router.delete("/:videoId", removeFavorite);
-
-// get all favorites
-router.get("/:userId", allFavorites);
 
 module.exports = router;
